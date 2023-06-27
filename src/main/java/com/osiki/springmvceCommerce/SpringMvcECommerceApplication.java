676cf339d9fd964419356c24fbc6161f7a1f7518
Segun Osiki <osiki.segun@gmail.com>
2023-06-27 20:57:22 +0100

    initial file

---
+++ src/main/java/com/osiki/springmvceCommerce/SpringMvcECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.osiki.springmvceCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osiki/springmvceCommerce/SpringMvcECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiki.springmvceCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
